45d7ed30d5eca5e98f441b7351e29eae3c97fe59
Kübra Türker <turkerkbr@gmail.com>
2025-08-08 14:00:31 +0300

    github workflow

--- conftest.py
+++ conftest.py
@@ -1,11 +1,25 @@
 import pytest
 from selenium import webdriver
+import tempfile
 
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
+
     options.add_argument("--start-maximized")
     options.add_argument('--disable-notifications')
+
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
+
+    temp_dir = tempfile.mkdtemp()
+    options.add_argument(f"--user-data-dir={temp_dir}")
+
+
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
